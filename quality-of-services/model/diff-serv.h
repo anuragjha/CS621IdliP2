2a6f61c92f698e7cf4a7ffe283713d4e03d9f077
vikas91 <ramaneti.vikas91@gmail.com>
2019-04-27 13:59:29 -0700

    Added diff serve to build

--- quality-of-services/model/diff-serv.h
+++ quality-of-services/model/diff-serv.h
@@ -11,32 +11,30 @@
 #include "ns3/traffic-class.h"
 #include <vector>
 
-using namespace ns3;
+namespace ns3 {
 
-class Diffserv : public ns3::Queue<ns3::Packet> {
-    public:
-    	enum QueueMode {
-    		QUEUE_MODE_PACKETS,
-			QUEUE_MODE_BYTES,
-    	};
-        static TypeId GetTypeId(void);
-        Diffserv();
-        virtual ~Diffserv();
+	class Diffserv : public ns3::Queue<ns3::Packet> {
+		public:
+			enum QueueMode {
+				QUEUE_MODE_PACKETS,
+				QUEUE_MODE_BYTES,
+			};
+			static TypeId GetTypeId(void);
+			Diffserv();
+			virtual ~Diffserv();
+			void SetMode(Diffserv::QueueMode mode);
+			Diffserv::QueueMode GetMode(void);
+			virtual Ptr<Packet> Schedule(void);
+			uint32_t Classify(Ptr<Packet> p);
+			std::vector<TrafficClass*> GetQ_Class(void);
+			TrafficClass GetTrafficClassAtIndex(int index);
 
-        void SetMode(Diffserv::QueueMode mode);
-        Diffserv::QueueMode GetMode(void);
-
-        virtual Ptr<Packet> Schedule(void);
-        uint32_t Classify(Ptr<Packet> p);
-
-        std::vector<TrafficClass*> GetQ_Class(void);
-        TrafficClass GetTrafficClassAtIndex(int index);
-
-    private:
-        QueueMode m_mode;
-        std::vector<TrafficClass*> q_class;
-        bool DoEnqueue(Ptr<Packet> p);
-        virtual Ptr<Packet> DoDequeue(void);
-        virtual Ptr<const Packet> DoPeek(void) const;
+		private:
+			QueueMode m_mode;
+			std::vector<TrafficClass*> q_class;
+			bool DoEnqueue(Ptr<Packet> p);
+			virtual Ptr<Packet> DoDequeue(void);
+			virtual Ptr<const Packet> DoPeek(void) const;
+	};
 }
 #endif //NS_3_ALLINONE_DIFF_SERV_H
